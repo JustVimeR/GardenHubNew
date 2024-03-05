c0563711b03f7836a45938d29326f449c36b6703
yarik tkachenko <yariktkachenko83@gmail.com>
2024-03-06 01:09:19 +0200

    update

--- GardenHub.Api/src/Libraries/Models/DbEntities/Feedback.cs
+++ GardenHub.Api/src/Libraries/Models/DbEntities/Feedback.cs
@@ -10,9 +10,11 @@ namespace Models.DbEntities
         public string? Text { get; set; }
 
         public long GardenerId { get; set; }
-        public long ProjectId { get; set; }
+        public long? CustomerId { get; set; }
+        public long? ProjectId { get; set; }
 
         public Project? Project { get; set; }
+        public CustomerProfile? Customer { get; set; }
         public GardenerProfile? Gardener { get; set; }
     }
 }
