c795b2ca4f9bf1309563a22ac2ac4cad35da4ac2
Vlad <dog792804@gmail.com>
2024-04-02 00:55:20 +0300

    add API to My Orders

--- GardenHub.Client/src/app/components/orders/orders-active/orders-active.component.ts
+++ GardenHub.Client/src/app/components/orders/orders-active/orders-active.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { OrderStatus } from 'src/app/models/enums/order-status';
 
 @Component({
@@ -7,30 +7,16 @@ import { OrderStatus } from 'src/app/models/enums/order-status';
   styleUrls: ['./orders-active.component.scss']
 })
 export class OrdersActiveComponent {
+  @Input() allProjects: any;
+
   toggleHeart(order: any) {
     order.isHeartClicked = !order.isHeartClicked;
   }
 
-  fakeOrders:any = [
-    {
-      title: 'Покосити газон на прибудинковій території',
-      location: 'м. Вишгород, Київська обл.',
-      price: '700',
-      isHeartClicked: false,
-      typeOfWork: [
-       'Догляд за газоном','Догляд за фруктовими деревами','Ландшафтний дизайн'
-      ],
-      orderStatus: OrderStatus.active
-    },
-    {
-      title: 'Обрізка фруктових дерев у саду',
-      location: 'м. Житомир',
-      price: 'Договірна',
-      isHeartClicked: false,
-      typeOfWork: [
-       'Догляд за фруктовими деревами','Ландшафтний дизайн','Догляд за газоном','Догляд за газоном'
-      ],
-      orderStatus: OrderStatus.active
+  getActiveProjects() {
+    if (!this.allProjects.data) {
+      return [];
     }
-  ]
+    return this.allProjects.data.filter((order: any) => order.status === 'Active');
+  }
 }
